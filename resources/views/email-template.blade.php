54d41baa9e4d8c6d6d58b56f7ca40bbedde3eeb8
fatimaMarzouq <fatima@softylus.com>
2022-02-02 18:02:10 +0200

    em

--- resources/views/email-template.blade.php
+++ resources/views/email-template.blade.php
@@ -3,7 +3,7 @@
 <h1 style="padding: 1rem; background: #f7f7f7;color:#EA84EB;">{{$Subject}}</h1>
     <p style="padding: 1rem;color:#EA84EB;font-size: 14px;">
         <!-- Welcome to Laravel. This is a demo of sending emails through
-        the Mailgun email service.https://ibb.co/9Ydkjmc -->
+        the Mailgun email service.https://ibb.co/9Ydkjmc a style="color:#EA84EB;font-size: 14px;" href='{{ url($Linked) }}'-->
         
         
         {{$Message}}
@@ -12,4 +12,4 @@
     
     <img style="padding: 1rem;width: 200px;" src="{{ asset('/assets/images/reviewss.png') }}">
  <div style="padding: 1rem;color:#EA84EB;font-size: 14px;">
-    <a style="color:#EA84EB;font-size: 14px;" href='{{ url($Linked) }}'>Click here for a review</a> {{$Linked}}</div>
+    <span>Click here for a review</span> {{$Linked}}</div>
